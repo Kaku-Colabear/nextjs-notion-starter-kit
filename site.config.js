b76522d4ee97130da2f016692461f6ebe025a801
Kaku-Colabear <95159151+Kaku-Colabear@users.noreply.github.com>
2021-11-28 15:12:14 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '179c9389f8de4e0c9b875f5e6c19a6f9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Resume Kaku',
+  domain: 'https://www.notion.so/Resume-Kaku-179c9389f8de4e0c9b875f5e6c19a6f9t',
+  author: 'Guo HaoHao',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,8 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  Instagram: 'colabear_kaku',
+  linkedin: 'www.linkedin.com/in/紫旋-郭-a8b616217',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
